8f74423450c6dbf41bc2b7ecac0780f0210baaa8
yu9i <yusun91317@gmail.com>
2023-05-16 17:19:13 +0900

    header

--- team8/src/App.vue
+++ team8/src/App.vue
@@ -4,45 +4,15 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    차공개 팀8 프론트 
-    <TheWelcome />
+    <div>left</div>
+    <div>middle</div>
+    <div>right</div>
   </main>
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+@import './assets/css/app.css'
 </style>
