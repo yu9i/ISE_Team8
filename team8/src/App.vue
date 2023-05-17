78d2482e2d36600b0fbccdc6fcc6b24e3f6ad4a9
yu9i <yusun91317@gmail.com>
2023-05-17 13:14:34 +0900

    main-page frame1

--- team8/src/App.vue
+++ team8/src/App.vue
@@ -4,15 +4,42 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <div class="main">
 
-  <main>
-    <div>left</div>
-    <div>middle</div>
-    <div>right</div>
-  </main>
+    <div class="main-menu">
+      <div class="main-menu-box">
+        <div>졸업작품/논문</div>
+        <div>인턴</div>
+        <div>1만 라인</div>
+        <div>산학협력프로젝트</div>
+        <div>기타</div>
+      </div>
+    </div>
+
+    <div class="main-posts">
+      <div id="page-name"><h1>졸업작품/논문</h1></div>
+      <div class="post">
+        <div id="post-title">제목</div>
+        <div class="post-content">
+          <div id="post-pic"></div>
+          <div id="post-det">내용</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-page">
+      <div class="page-select">
+        <div>Home</div>
+        <div>Team</div>
+        <div></div>
+      </div>
+      <div class="main-show-profile">profile</div>
+      <div id="add-post">글 추가</div>
+    </div>
+
+  </div>
   
 </template>
 
 <style scoped>
-@import './assets/css/app.css'
 </style>
