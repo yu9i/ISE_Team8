f510da6f1a644e90289552551f159d1a546385be
fjaoief <darthgun@naver.com>
2023-05-19 20:28:01 +0900

    마이페이지 완성

--- team8/src/components/MyPage.vue
+++ team8/src/components/MyPage.vue
@@ -33,8 +33,73 @@
                     <label for="인턴십">인턴십</label>
                 </td>
             </tr>
-        </table>
-        
+        </table><br>
+        <div>사용 기록</div>
+        <div class="main-posts">
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문2</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문3</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -53,4 +118,39 @@
     text-align: center;
     background-color: azure;
 }
+.main-posts {
+    margin-left: auto;
+    margin-right: auto;
+    background: #ECECEC;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  text-align: center;
+  align-items: center;
+  width: 50%;
+  max-height: 200px;
+  margin-bottom: 3em;
+  padding-bottom: 2em;
+  overflow: scroll;
+}
+.main-posts > #page-name {
+  padding: 0.5em;
+}
+.main-posts > .post {
+    margin-left: auto;
+    margin-right: auto;
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  background: white;
+  border: solid 1px black;
+  border-radius: 10px;
+  padding: 5px;
+  margin: 8px 0;
+  cursor: pointer;
+}
+
+
 </style>
