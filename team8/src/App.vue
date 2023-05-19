e41e3ef723ea9845b6d5b7ae27a1c213605ad6ae
yu9i <yusun91317@gmail.com>
2023-05-19 13:13:50 +0900

    commu2

--- team8/src/App.vue
+++ team8/src/App.vue
@@ -5,69 +5,126 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <div class="main">
+    <div class="main-community">
+      <div class="main-menu">
+        <div class="main-menu-box">
+          <div>졸업작품/논문</div>
+          <div>인턴</div>
+          <div>1만 라인</div>
+          <div>산학협력프로젝트</div>
+          <div>기타</div>
+        </div>
+      </div>
 
-    <div class="main-menu">
-      <div class="main-menu-box">
-        <div>졸업작품/논문</div>
-        <div>인턴</div>
-        <div>1만 라인</div>
-        <div>산학협력프로젝트</div>
-        <div>기타</div>
+      <div class="main-posts">
+        <div id="page-name"><h1>졸업작품/논문</h1></div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문2</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문3</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+        <div class="post">
+          <div id="post-title">졸업 작품 관련 질문4</div>
+          <div class="post-content">
+            <div id="post-pic"></div>
+            <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="main-page">
+        <div class="page-home" Onclick="">Home</div>
+        <div class="page-team" Onclick="">Team</div>
+        <div class="page-profile">
+          <div class="page-profile-top">
+            <div id="page-profile-pic"></div>
+            <div id="page-profile-name">이름</div>
+          </div>
+          <div class="page-profile-mid">
+            <div id="page-profile-dep">소프트웨어학과</div>
+            <div id="page-profile-grad">2025년 2월 졸업 예정</div>
+          </div>
+          <div class="page-profile-bottom" Onclick="">내 프로필 보러가기</div>
+        </div>
+        <div class="main-add-postb" @click="AddPost()"><font-awesome-icon icon="fa-solid fa-square-plus" size="3x"/></div>
       </div>
     </div>
 
-    <div class="main-posts">
-      <div id="page-name"><h1>졸업작품/논문</h1></div>
-      <div class="post">
-        <div id="post-title">졸업 작품 관련 질문</div>
-        <div class="post-content">
-          <div id="post-pic"></div>
-          <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
+    <div class="commu-add-post">
+      <div class="commu-add-new">
+        <div class="commu-add-title">
+          <input type="text" id="commu-add-title" name="" required minlength="2" maxlength="15" placeholder="제목을 입력하세요">
         </div>
-      </div>
-      <div class="post">
-        <div id="post-title">졸업 작품 관련 질문2</div>
-        <div class="post-content">
-          <div id="post-pic"></div>
-          <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
-        </div>
-      </div>
-      <div class="post">
-        <div id="post-title">졸업 작품 관련 질문3</div>
-        <div class="post-content">
-          <div id="post-pic"></div>
-          <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
-        </div>
-      </div>
-      <div class="post">
-        <div id="post-title">졸업 작품 관련 질문4</div>
-        <div class="post-content">
-          <div id="post-pic"></div>
-          <div id="post-det">안녕하세요. 저는 소프트웨어학과 ㅁㅁ학번 학생 입니다. 곧, 중간보고서 제출 기간이어서 중간보고서를 작성하고 있는데 궁금한 점이 몇 개 있습니다. 보고서 양식을 보면 서명란이 있는데 교수님께 작접 서명을 안 받아도 되는 것이 맞나요? 그리고 제출할 때 파일 양식은 따로 정해져 있나요? 제출 양식 관련 내용이 안 적혀져 있어서 질문 드립니다.</div>
-        </div>
+        <div class="commu-add-det">내용</div>
+        <div class="commu-add-file">파일</div>
+        <div class="commu-add-all">추가하기</div>
       </div>
     </div>
-
-    <div class="main-page">
-      <div class="page-home" Onclick="">Home</div>
-      <div class="page-team" Onclick="">Team</div>
-      <div class="page-profile">
-        <div class="page-profile-top">
-          <div id="page-profile-pic"></div>
-          <div id="page-profile-name">이름</div>
-        </div>
-        <div class="page-profile-mid">
-          <div id="page-profile-dep">소프트웨어학과</div>
-          <div id="page-profile-grad">2025년 2월 졸업 예정</div>
-        </div>
-        <div class="page-profile-bottom" Onclick="">내 프로필 보러가기</div>
-      </div>
-      <div Onclick=""><font-awesome-icon icon="fa-solid fa-square-plus" size="3x"/></div>
-    </div>
-
   </div>
   
 </template>
+
+<script>
+  export default {
+    methods:{
+      AddPost(){
+
+      }
+    }
+  }
+</script>
+
 
 <style scoped>
 
